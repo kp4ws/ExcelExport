4982fa5d190337e4aef1e0eee1264f1467c3635b
kpawson <kent.pawson@avan-tech.ca>
2021-09-15 13:47:06 -0600

    Create table now supports dynamic table names

--- extract_excel_create.py
+++ extract_excel_create.py
@@ -19,8 +19,9 @@ class ExtractExcelUpdate:
         
         print('Rows:')
         self._rows = int(input())
-        print('Columns:')
-        self._columns = int(input())
+        #print('Columns:')
+        #self._columns = int(input())
+        self._columns = 4
 
         #table delimiter
         print('Enter delimiter')
@@ -32,22 +33,34 @@ class ExtractExcelUpdate:
     #Export data from excel to sql file
     def exportData(self):
         with open (self._directory, 'w') as f:
-            for i in range(2, self._rows+1):
-                if(i == 2):
-                    f.write('CREATE TABLE table_name (\n')
 
-                data_str = ''
-                delim_check = str(self._ws.cell(row=i+1, column=1).value).strip()
+            #initial CREATE TABLE statement (currently being hardcoded)
+            f.write('CREATE TABLE ' + str(self._ws.cell(row=2, column=1).value).strip() + '(\n')
+            for i in range(2, self._rows+1):
                 
-                for j in range(1, self._columns + 1):
+                #Reset data string per iteration of the loop
+                data_str = ''
+                #Check for delimiter on next line/row
+                delim_check = str(self._ws.cell(row=i+1, column=2).value).strip()
+                
+                for j in range(2, self._columns + 1):
+
+                    #Skip delimiter line
                     if(self._ws.cell(row=i, column=j).value == self._delimiter):
                         break
+
+                    #If constraint column empty, don't append anything to this line
                     elif(self._ws.cell(row=i,column=j).value is not None):
                         data_str += str(self._ws.cell(row=i, column=j).value).strip() + ' '
 
                 else:
+                    #Check for when next CREATE TABLE should happen.
+                    #NOTE, We need to check this in advance, otherwise a comma will be added to the last table column which is incorrect
                     if(delim_check == self._delimiter):
-                        f.write(data_str + '\n);\n\nCREATE TABLE table_name (\n')
+                        if(self._ws.cell(row=i+1, column=1).value is not None):
+                            f.write(data_str + '\n);\n\nCREATE TABLE ' + str(self._ws.cell(row=i+1, column=1).value).strip() + ' (\n')
+                        else:
+                            f.write(data_str + '\n);\n\nCREATE TABLE table_name (\n')
                     else:
                         f.write(data_str[:-1] + ',\n')
             else:
