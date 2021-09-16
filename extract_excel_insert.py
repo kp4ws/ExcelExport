af527f4076b440f991da747d33d1d24b3754549e
kp4ws <kent.pawson@avan-tech.ca>
2021-09-16 12:47:18 -0600

    Working on JSON config file

--- extract_excel_insert.py
+++ extract_excel_insert.py
@@ -1,39 +1,67 @@
 import openpyxl
 from openpyxl import load_workbook
 from datetime import datetime
+import json
 
 class ExtractExcelInsert:
+    FILE_NAME = '.\excel_config.json'
 
     #Constructor
     def __init__(self):
+        #Get data from JSON config file
+        self._config = self.retrieveData()
+        
+        #Initialize variables
+        self._wb_name = ''
+        self._directory = ''
+        self._rows = ''
+        self._columns = ''
+        self._col_start = ''
 
-        #Get user input
-        print('Enter absolute path for Workbook "C:\\directory name\\<file name>.xlsx"')
-        self._wb_name = input()
-        #self._wb = load_workbook('.\\'+self._wb_name+'.xlsx', data_only=True)
+        if(str(self._config) != '{}' and input('Use recent config? (Enter = yes, Value = no) ') == ''):
+            self._wb_name = self._config['workbook']
+            self._directory = self._config['destination']
+            self._rows = self._config['rows']
+            self._columns = self._config['columns']
+            self._col_start = self._config['start']
+        else:
+            self.userInput()
+            self.saveConfig()
+        
         self._wb = load_workbook(self._wb_name, data_only=True)
         self._ws = self._wb.active
-
-        print('Enter absolute path for Export Location "C:\\directory name\\<file name>.sql"')
-        self._directory = input()
-        
-        print('Rows:')
-        self._rows = int(input())
-        print('Columns:')
-        self._columns = int(input())
-
-        #column to start at
-        print('Enter column start')
-        self._col_start = int(input())
-
-    	#Primary method for exporting data
         self.exportData()
+
+    #Get data to export
+    def retrieveData(self):
+        with open(self.FILE_NAME) as f:
+            return json.load(f)
+
+    def userInput(self):
+        self._wb_name = input('Enter absolute path for Workbook "C:\\directory name\\<file name>.xlsx" ')
+        self._directory = input('Enter absolute path for Export Location "C:\\directory name\\<file name>.sql" ')
+        self._rows = int(input('Rows: '))
+        self._columns = int(input('Columns: '))
+        self._col_start = int(input('Enter column start '))
+
+    def saveConfig(self):
+        new_config = {
+            "workbook" : self._wb_name,
+            "destination" : self._directory,
+            "rows" : self._rows,
+            "columns" : self._columns,
+            "start" : self._col_start
+        }
+
+        json_obj = json.dumps(new_config, indent = 6)
+        with open(self.FILE_NAME, "w") as f:
+            f.write(json_obj)
 
     #Export data from excel to sql file
     def exportData(self):
         with open (self._directory, 'w') as f:
             for i in range(2, self._rows+1):
-                f.write('INSERT INTO table_name VALUES(');
+                f.write('INSERT INTO table_name VALUES(')
                 data_str = ''
 
                 for j in range(self._col_start, self._columns + self._col_start):
